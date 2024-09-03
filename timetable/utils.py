3677a5ecb6bc1e4530f07e76b8cfb92e1a518177
nova <novanai.dev@gmail.com>
2024-09-03 18:42:56 +0100

    Fix semester removal regex

--- timetable/utils.py
+++ timetable/utils.py
@@ -82,7 +82,7 @@ class EventDisplayData:
         # SUMMARY
 
         name = (
-            re.sub(r"[\[\(][0-2F][\]\)]", "", n, 1)
+            re.sub(r"[\[\(][0-2F,]+[\]\)]", "", n, 1)
             if (n := event.module_name)
             else event.name
         )
