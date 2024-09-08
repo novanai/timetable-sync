c44d0a04ed778eaffc32aa96f46ee0c336bdb5b8
nova <novanai.dev@gmail.com>
2024-09-08 18:24:23 +0100

    Add time options

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import os
 
 import arc
 import hikari
+import parsedatetime
 
 from timetable import api as api_
 from timetable import utils
@@ -9,6 +10,9 @@ from timetable import utils
 bot = hikari.GatewayBot(os.environ["BOT_TOKEN"], banner=None)
 client = arc.GatewayClient(bot)
 
+client.set_type_dependency(
+    parsedatetime.Calendar, parsedatetime.Calendar(parsedatetime.Constants("en_GB"))
+)
 client.set_type_dependency(api_.API, api_.API())
 client.load_extension("bot.timetable")
 
