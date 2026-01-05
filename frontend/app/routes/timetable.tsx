8c2a43ca3c0efc2616b1d926328fcebc7a9387d0
nova <novanai.dev@gmail.com>
2026-01-05 16:23:35 +0000

    replace fullcalendar with react-big-calendar, add support for modules and locations

--- frontend/app/routes/timetable.tsx
+++ frontend/app/routes/timetable.tsx
@@ -1,158 +1,153 @@
-import type { Route } from "./+types/timetable";
-import { useState, useRef } from "react";
-import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
-import iCalendarPlugin from "@fullcalendar/icalendar";
-import momentTimezonePlugin from "@fullcalendar/moment-timezone";
+import type { Route } from "./+types/timetable_new";
+import { useState, useEffect } from "react";
 import AsyncSelect from 'react-select/async';
-import MultiValue from 'react-select';
-
-
-// import ColorHash from 'color-hash';
-
-// import * as ColorHashModule from "color-hash";
-// const ColorHash = (ColorHashModule as any).default || (ColorHashModule as any).ColorHash;
+import type { MultiValue } from 'react-select';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import type { Event } from "react-big-calendar";
+import moment from "moment";
 
 import ColorHashModule from "color-hash";
-const ColorHash = ColorHashModule.default || ColorHashModule;  // I don't know why this works
-
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
-type Option = {
-  label: string;
-  value: string;
-}
+const ColorHash = ColorHashModule.default || ColorHashModule;
 
 var colorHash = new ColorHash({ lightness: [0.6, 0.7, 0.8], saturation: [0.4, 0.5, 0.6] });
 
-const loadOptions = async (inputValue: string): Promise<Option[]> => {
-  if (!inputValue) return [];
+moment.updateLocale('en-GB', {
+    week: {
+        dow: 1,
+        doy: 4,
+    }
+})
 
-  const res = await fetch(`http://localhost/api/all/course?query=${inputValue}`);
-  const data = await res.json();
+const localizer = momentLocalizer(moment);
 
-  return data.map((item: any) => ({
-    label: item.name,
-    value: item.identity,
-  }));
+export function meta({ }: Route.MetaArgs) {
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+}
+
+type Option = {
+    label: string;
+    value: string;
+}
+
+type SelectConfig = {
+    id: string;
+    value: readonly Option[];
+    setValue: React.Dispatch<React.SetStateAction<readonly Option[]>>;
 };
 
+const createLoadOptions = (type: string) => {
+    return async (inputValue: string): Promise<Option[]> => {
+        if (!inputValue) return [];
+
+        const res = await fetch(`http://localhost/api/all/${type}?query=${inputValue}`);
+        const data = await res.json();
+
+        return data.map((item: any) => ({
+            label: item.name,
+            value: item.identity,
+        }));
+    };
+};
+
+function parseCalendarJSON(data: any[]): Event[] {
+    return data.map(event => ({
+        title: event.display.summary,
+        start: new Date(event.start),
+        end: new Date(event.end),
+        allDay: false,
+
+        resource: event,
+    }));
+}
+
 export default function Timetable() {
-  const [courseIds, setCourseIds] = useState(new Set<string>());
-  const calendarRef = useRef<FullCalendar | null>(null);
+    const [events, setEvents] = useState<Event[]>([]);
 
-  const handleSelectChange = (courseIdsSelection: MultiValue<Option>) => {
-    let tempCourseIds: Set<string> = new Set(courseIdsSelection.map((option: Option) => option.value))
+    const [courseOptions, setCourseOptions] = useState<readonly Option[]>([]);
+    const [moduleOptions, setModuleOptions] = useState<readonly Option[]>([]);
+    const [locationOptions, setLocationOptions] = useState<readonly Option[]>([]);
 
-    function diffSets(oldSet: Set<string>, newSet: Set<string>) {
-      const added = [...newSet].filter(x => !oldSet.has(x));
-      const removed = [...oldSet].filter(x => !newSet.has(x));
-      return { added, removed };
+    const selects: Array<SelectConfig> = [
+        { id: "course", value: courseOptions, setValue: setCourseOptions },
+        { id: "module", value: moduleOptions, setValue: setModuleOptions },
+        { id: "location", value: locationOptions, setValue: setLocationOptions },
+    ]
+
+    async function fetchCalenar() {
+        const courses = Array.from(courseOptions.map(o => o.value)).join(",");
+        const modules = Array.from(moduleOptions.map(o => o.value)).join(",");
+        const locations = Array.from(locationOptions.map(o => o.value)).join(",");
+
+        const res = await fetch(`http://localhost/api/?courses=${courses}&modules=${modules}&locations=${locations}&format=json&display=true`);
+        return await res.json();
     }
 
-    const diff = diffSets(courseIds, tempCourseIds);
-    setCourseIds(tempCourseIds)
-    
-    diff.removed.map((courseId: string) => calendarRef.current?.getApi().getEventSourceById(courseId)?.remove())
-    diff.added.map((courseId: string) => {calendarRef.current?.getApi().addEventSource({
-      id: courseId,
-      url: `http://localhost/api/?courses=${courseId}`,
-      format: "ics"
-    })})
-  };
+    useEffect(() => {
+        const loadEvents = async () => {
+            const data = await fetchCalenar();
+            setEvents(parseCalendarJSON(data));
+        };
 
-  const setEventColour = (info) => {
-    const colour = colorHash.hex(
-      (info.event.title.split(" ")[0]),
-    )
-    info.el.style.backgroundColor = colour;
-  }
-
-  const renderEventContent = (arg) => {
-     const { event } = arg;
-    const { description, location } = event.extendedProps as {
-      description?: string;
-      location?: string;
-    };
+        loadEvents();
+    }, [courseOptions, moduleOptions, locationOptions]);
 
     return (
-      <div>
-        <strong>{event.title}</strong>
-        {location && <div>{location}</div>}
-        {description && (
-          <div
-            // style={{
-            //   fontSize: 'smaller',
-            //   color: 'gray',
-            //   whiteSpace: 'pre-line',
-            //   overflow: 'hidden',
-            //   textOverflow: 'ellipsis',
-            // }}
-          >
-            {description.split('\n').map((line, i) => (
-              <span>
-                {line}
-                <br />
-              </span>
-            ))}
-          </div>
-        )}
-      </div>
+        <main>
+            <Tabs>
+                <TabList>
+                    <Tab>Courses</Tab>
+                    <Tab>Modules</Tab>
+                    <Tab>Locations</Tab>
+                </TabList>
+
+                {selects.map(({ id, value, setValue }) => (
+                    <TabPanel key={id}>
+                        <AsyncSelect
+                            isMulti
+                            name={`select-${id}`}
+                            cacheOptions
+                            loadOptions={createLoadOptions(id)}
+                            value={value}
+                            onChange={setValue}
+                            styles={{
+                                menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                            }}
+                        />
+                    </TabPanel>
+                ))}
+            </Tabs>
+
+            <Calendar
+                localizer={localizer}
+                defaultDate={new Date()}
+                defaultView="week"
+                min={new Date(0, 0, 0, 8, 0)}
+                max={new Date(0, 0, 0, 19, 0)}
+                style={{ height: "960px" }}
+                events={events}
+                eventPropGetter={(event) => {
+                    const backgroundColor = colorHash.hex((event.title as string).split(" ")[0]);
+
+                    return {
+                        style: {
+                            backgroundColor,
+                        },
+                    };
+                }}
+                components={{
+                    event: ({ event }) => (
+                        <div>
+                            <p><b>{event.title}</b></p>
+                            <p>📄 {event.resource.display.description}</p>
+                            <p>📍 {event.resource.display.location}</p>
+                        </div>
+                    )
+                }}
+            />
+        </main>
     );
-  }
-
-
-  return (
-    <main>
-      <AsyncSelect
-        isMulti
-        name="course-select"
-        cacheOptions
-        loadOptions={loadOptions}
-        defaultOptions
-        onChange={handleSelectChange}
-        menuPortalTarget={typeof document !== "undefined" ? document.body : null}  // bc of ssr
-        styles={{
-          menuPortal: base => ({ ...base, zIndex: 9999 })
-        }}
-      />
-
-      <FullCalendar
-        ref={calendarRef}
-        plugins={[timeGridPlugin, listPlugin, momentTimezonePlugin, iCalendarPlugin]}
-        initialView="timeGridWeek"
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "timeGridWeek,timeGridDay,listWeek",
-        }}
-        height="80vh"
-        locale="en-GB"
-        timeZone="Europe/Dublin"
-        slotLabelFormat={{
-            hour: "numeric",
-            minute: "numeric"
-        }}
-        allDaySlot={false}
-        weekends={true}
-        slotMinTime="08:00:00"
-        slotMaxTime="19:00:00"
-        editable={false}
-        firstDay={1} // Monday
-        nowIndicator={true}
-        slotDuration="01:00:00"
-        expandRows={true}
-        eventDidMount={setEventColour}
-        eventContent={renderEventContent}
-        eventTextColor={"#000"}
-        eventBorderColor={"#0000"}
-      />
-    </main>
-  );
 }
