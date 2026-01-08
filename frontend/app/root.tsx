2362bebf3acaf943d2700ad0e2210f13838451ac
nova <novanai.dev@gmail.com>
2026-01-08 20:29:37 +0000

    add themes and theme switcher

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
 import {
   isRouteErrorResponse,
   Links,
@@ -23,7 +25,16 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+const themes = [
+  { name: "Light", value: "winter" },
+  { name: "Dark", value: "night" },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    themeChange(false)
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -33,6 +44,34 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <div className="dropdown mb-72">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Theme
+            <svg
+              width="12px"
+              height="12px"
+              className="inline-block h-2 w-2 fill-current opacity-60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048">
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <ul tabIndex="-1" className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
+            {
+              themes.map(({ name, value }) => (
+                <li>
+                  <input
+                    type="radio"
+                    name="theme-dropdown"
+                    className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                    aria-label={name}
+                    data-set-theme={value} />
+                </li>
+              ))
+            }
+
+          </ul>
+        </div>
         {children}
         <ScrollRestoration />
         <Scripts />
