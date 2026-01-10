0c958c1da4d449f0443ad950a5977c29b3565b7b
nova <novanai.dev@gmail.com>
2026-01-10 19:35:52 +0000

    rename files

--- backend/src/v2/routes.py
+++ backend/src/v2/routes.py
@@ -1,20 +1,52 @@
 import collections
 import datetime
-from typing import Annotated
+import os
 
-import msgspec
-from fastapi import APIRouter, Depends, HTTPException, Path, Query, Response
+import blacksheep
+import orjson
+from blacksheep.server.openapi.v3 import OpenAPIHandler
+from openapidocs.v3 import Info  # pyright: ignore[reportMissingTypeStubs]
+from timetable import api as api_
 from timetable import cns, models, utils
-from timetable.api import API as TimetableAPI  # noqa: N811
-from timetable.cns import API as CNSAPI
 
-from src.dependencies import get_cns_api, get_timetable_api
+from src import __version__, api_docs
 
-router = APIRouter()
+app = blacksheep.Application()
+api = api_.API(os.environ["REDIS_ADDRESS"])
+cns_api = cns.API(os.environ["CNS_ADDRESS"])
+
+docs = OpenAPIHandler(
+    info=Info(title="TimetableSync API", version=__version__),
+    ui_path="/api/api_docs",
+    json_spec_path="/api/openapi.json",
+)
+docs.bind_app(app)
+
+
+@app.on_start
+async def start_session() -> None:
+    for category_type in (
+        models.CategoryType.PROGRAMMES_OF_STUDY,
+        models.CategoryType.MODULES,
+        models.CategoryType.LOCATIONS,
+    ):
+        await utils.get_basic_category_results(api, category_type)
+
+
+@app.on_stop
+async def stop_session() -> None:
+    await api.session.close()
+    await cns_api.session.close()
 
 
 def str_to_list(text: str) -> list[str]:
     return [t.strip() for t in text.split(",")]
+
+
+@docs.ignore()
+@blacksheep.route("/api/healthcheck")
+async def healthcheck() -> blacksheep.Response:
+    return blacksheep.ok()
 
 
 CATEGORY_TYPES: dict[str, models.CategoryType] = {
@@ -24,125 +56,56 @@ CATEGORY_TYPES: dict[str, models.CategoryType] = {
 }
 
 
-@router.get("/all/{category_type}")
-async def get_all_category_items(
-    timetable_api: Annotated[TimetableAPI, Depends(get_timetable_api)],
-    cns_api: Annotated[CNSAPI, Depends(get_cns_api)],
-    category_type: Annotated[str, Path()],
-    query: Annotated[str | None, Query()] = None,
-) -> Response:
+@docs.ignore()
+@blacksheep.route("/api/all/{category_type}")
+async def all_category_values(
+    category_type: str, query: str | None = None
+) -> blacksheep.Response:
     if category_type not in ("course", "module", "location", "club", "society"):
-        raise HTTPException(status_code=400, detail="Invalid value provided.")
+        return blacksheep.status_code(
+            400,
+            "Invalid value provided.",
+        )
 
     if category_type in CATEGORY_TYPES:
-        category = await timetable_api.get_category(
-            CATEGORY_TYPES[category_type],
-            query=query,
-            items_type=models.BasicCategoryItem,
+        categories = await utils.get_basic_category_results(
+            api, CATEGORY_TYPES[category_type], query
         )
-        if not category:
-            category = await timetable_api.fetch_category(
-                CATEGORY_TYPES[category_type],
-                query=query,
-                items_type=models.BasicCategoryItem,
-            )
-
-        items = category.items
 
     else:
-        group_type = cns.GroupType(category_type)
-        items = await cns_api.get_group_items(group_type, query)
-        if not items:
-            items = await cns_api.fetch_group_items(group_type, query)
+        categories = await cns_api.fetch_unlocked_groups(cns.GroupType(category_type))
+        if query:
+            categories = cns.filter_category_results(categories, query)
 
-        items = [{"name": item.name, "identity": item.id} for item in items]
-
-    return Response(
-        content=msgspec.json.encode(items),
-        media_type="application/json",
+    return blacksheep.Response(
+        status=200,
+        content=blacksheep.Content(
+            content_type=b"application/json",
+            data=orjson.dumps(categories),
+        ),
     )
 
 
-@router.get(
-    "/",
-    summary="Generate a timetable.",
-    description="One of 'course', 'courses' or 'modules' must be provided, but not both. All other parameters are optional.",
-)
-async def get_calendar_events(
-    timetable_api: Annotated[TimetableAPI, Depends(get_timetable_api)],
-    course: Annotated[  # NOTE: backwards compatibility only
-        str | None,
-        Query(
-            description="The course to generate a timetable for.",
-            examples=["COMSCI1"],
-            deprecated=True,
-        ),
-    ] = None,
-    courses: Annotated[
-        str | None,
-        Query(
-            description="The course(s) to generate a timetable for.",
-            examples=["COMSCI1,COMSCI2"],
-        ),
-    ] = None,
-    modules: Annotated[
-        str | None,
-        Query(
-            description="The module(s) to generate a timetable for.",
-            examples=["CSC1061,CSC1003,MTH1025"],
-        ),
-    ] = None,
-    locations: Annotated[
-        str | None,
-        Query(
-            description="The location(s) to generate a timetable for.",
-            examples=["GLA.SA101,SPC.E203"],
-        ),
-    ] = None,
-    _format: Annotated[
-        str | None,
-        Query(
-            alias="format",
-            description="The response format.\n\nAllowed values: 'ical' or 'json'.\nDefault: 'ical'.",
-            examples=["json"],
-        ),
-    ] = None,
-    display: Annotated[
-        bool | None,
-        Query(
-            description="Deprecated. Display details are included regardless.\n\nWhether or not to include additional display info.",
-            examples=["true"],
-            deprecated=True,
-        ),
-    ] = None,
-    start: Annotated[
-        str | None,
-        Query(
-            description="Only get timetable events later than this datetime.",
-            examples=["2023-10-31T13:00:00"],
-        ),
-    ] = None,
-    end: Annotated[
-        str | None,
-        Query(
-            description="Only get timetable events earlier than this datetime.",
-            examples=["2024-04-23T10:00:00"],
-        ),
-    ] = None,
-) -> Response:
-    if _format is None:
-        _format = "ical"
-
-    if _format not in {"ical", "json"}:
-        raise HTTPException(status_code=400, detail=f"Invalid format '{_format}'.")
-
-    if not course and not courses and not modules and not locations:
-        raise HTTPException(
-            status_code=400, detail="No courses, modules or locations provided."
-        )
-
+@docs(api_docs.API)
+@blacksheep.route("/api")
+async def timetable_api(
+    course: str | None = None,  # NOTE: backwards compatibility only
+    courses: str | None = None,
+    modules: str | None = None,
+    locations: str | None = None,
+    format: str | None = None,
+    display: bool | None = None,
+    start: str | None = None,
+    end: str | None = None,
+) -> blacksheep.Response:
+    format_ = models.ResponseFormat.from_str(format if format else None)
     start_date = datetime.datetime.fromisoformat(start) if start else None
     end_date = datetime.datetime.fromisoformat(end) if end else None
+
+    if not course and not courses and not modules and not locations:
+        raise ValueError("No courses, modules or locations provided.")
+    if format_ is models.ResponseFormat.UNKNOWN:
+        raise ValueError(f"Invalid format '{format_}'.")
 
     codes: dict[models.CategoryType, list[str]] = collections.defaultdict(list)
 
@@ -155,62 +118,60 @@ async def get_calendar_events(
     if locations:
         codes[models.CategoryType.LOCATIONS].extend(str_to_list(locations))
 
-    items = await utils.resolve_to_category_items(codes, timetable_api)
+    items = await utils.resolve_to_category_items(codes, api)
     identities = {
         group: [item.identity for item in group_items]
         for group, group_items in items.items()
     }
-    events = await utils.gather_events(identities, start_date, end_date, timetable_api)
+    events = await utils.gather_events(identities, start_date, end_date, api)
 
-    if _format == "ical":
-        timetable = utils.to_ics_file(events)
-        return Response(
-            content=timetable,
-            media_type="text/calendar",
-        )
-    assert _format == "json"
-    return Response(
-        content=msgspec.json.encode(events),
-        media_type="application/json",
+    if format_ is models.ResponseFormat.ICAL:
+        timetable = utils.generate_ical_file(events)
+    else:
+        assert format_ is models.ResponseFormat.JSON
+        timetable = utils.generate_json_file(events, display)
+
+    return blacksheep.Response(
+        200,
+        content=blacksheep.Content(
+            format_.content_type.encode(),
+            data=timetable,
+        ),
     )
 
 
-@router.get("/cns")
-async def get_cns_calendar_events(
-    cns_api: Annotated[CNSAPI, Depends(get_cns_api)],
-    societies: Annotated[str | None, Query()] = None,
-    clubs: Annotated[str | None, Query()] = None,
-) -> Response:
+@blacksheep.route("/api/cns")
+async def cns_api_timetable(
+    societies: str | None = None,
+    clubs: str | None = None,
+):
     if not (societies or clubs):
-        raise HTTPException(status_code=400, detail="No societies or clubs provided.")
+        raise ValueError("No societies or clubs provided.")
 
     society_ids = str_to_list(societies) if societies else []
     club_ids = str_to_list(clubs) if clubs else []
 
-    events: dict[str, list[cns.Event | cns.Activity | cns.Fixture]] = (
-        collections.defaultdict(list)
-    )
+    events: dict[str, list[cns.Event | cns.Activity]] = collections.defaultdict(list)
 
     for group_type, ids in (
         (cns.GroupType.SOCIETY, society_ids),
         (cns.GroupType.CLUB, club_ids),
     ):
-        for item_id in ids:
-            item = await cns_api.get_item(item_id)
-            if not item:
-                item = await cns_api.fetch_item(group_type, item_id)
-
-            events_ = await cns_api.get_group_events_activities_fixtures(item_id)
-            if events_ is None:
-                events_ = await cns_api.fetch_group_events_activities_fixtures(
-                    group_type, item_id
-                )
-
-            events[item.name].extend(events_)
+        for id_ in ids:
+            group = await cns_api.fetch_group_info(group_type, id_)
+            events[group.name].extend(
+                await cns_api.fetch_group_events_activities(group_type, id_)
+            )
 
     calendar = cns.generate_ical_file(events)
 
-    return Response(
-        content=calendar,
-        media_type="text/calendar",
+    return blacksheep.Response(
+        200,
+        content=blacksheep.Content(
+            b"text/calendar",
+            data=calendar,
+        ),
     )
+
+
+# TODO: add error handler
