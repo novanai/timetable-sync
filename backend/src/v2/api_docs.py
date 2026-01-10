e322652f7c469b6695e46bb7c447c560b2d67495
nova <novanai.dev@gmail.com>
2026-01-10 19:35:12 +0000

    move v2 api to new directory

---
+++ backend/src/v2/api_docs.py
@@ -0,0 +1,158 @@
+import datetime
+
+from blacksheep.server.openapi.common import (
+    ContentInfo,
+    EndpointDocs,
+    ParameterInfo,
+    ResponseInfo,
+)
+
+from timetable import models
+
+API = EndpointDocs(
+    summary="Generate a timetable.",
+    description="One of 'course', 'courses' or 'modules' must be provided, but not both. All other parameters are optional.",
+    parameters={
+        "course": ParameterInfo(
+            "The course to generate a timetable for.",
+            str,
+            required=False,
+            example="COMSCI1",
+        ),
+        "courses": ParameterInfo(
+            "The course(s) to generate a timetable for.",
+            str,
+            required=False,
+            example="COMSCI1,COMSCI2",
+        ),
+        "modules": ParameterInfo(
+            "The module(s) to generate a timetable for.",
+            str,
+            required=False,
+            example="CSC1061,CSC1003,MTH1025",
+        ),
+        "format": ParameterInfo(
+            "The response format.\n\nAllowed values: 'ical' or 'json'.\nDefault: 'ical'.",
+            str,
+            required=False,
+            example="json",
+        ),
+        "display": ParameterInfo(
+            "Whether or not to include additional display info",
+            bool,
+            required=False,
+            example="true",
+        ),
+        "start": ParameterInfo(
+            "Only get timetable events later than this datetime.",
+            str,
+            required=False,
+            example="2023-10-31T13:00:00",
+        ),
+        "end": ParameterInfo(
+            "Only get timetable events earlier than this datetime.",
+            str,
+            required=False,
+            example="2024-04-23T10:00:00",
+        ),
+    },
+    responses={
+        200: ResponseInfo(
+            (
+                "Successfully generated a timetable. However, it is not guaranteed to actually contain any events.\n\n"
+                "If format was 'json', response will be in json format. Otherwise if format was 'ical' or not specified, "
+                "response will be in plain text."
+            ),
+            content=[
+                ContentInfo(
+                    str,
+                    examples=[
+                        """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//nova@redbrick.dcu.ie//TimetableSync//EN
+METHOD:PUBLISH
+BEGIN:VEVENT
+SUMMARY:CSC1003 Computer Programming I (Lecture)
+DTSTART:20230925T090000Z
+DTEND:20230925T110000Z
+DTSTAMP:20231030T154328Z
+UID:aac10915-54ec-4128-996e-3e371dd39d42
+CLASS:PUBLIC
+DESCRIPTION:Lecture\\, On Campus
+LAST-MODIFIED:20231012T112440Z
+LOCATION:HG23 (Nursing Building\\, Glasnevin)
+END:VEVENT
+END:VCALENDAR
+"""
+                    ],
+                    content_type="text/plain",
+                ),
+                # TODO: update this to include display properties if available
+                ContentInfo(
+                    list[models.Event],
+                    examples=[
+                        [
+                            models.Event(
+                                identity="30658267-d87b-4dfc-9086-ae594bf1de1c",
+                                start=datetime.datetime.fromisoformat(
+                                    "2023-09-28T10:00:00+00:00"
+                                ),
+                                end=datetime.datetime.fromisoformat(
+                                    "2023-09-28T11:00:00+00:00"
+                                ),
+                                status_identity="119e2af6-701c-4442-8230-a5c5020f308e",
+                                locations=[
+                                    models.Location(
+                                        campus="GLA",
+                                        building="Q",
+                                        floor="G",
+                                        room="15",
+                                        original=None,
+                                    ),
+                                ],
+                                description="Lecture",
+                                name="CSC1003[1]OC/L1/01",
+                                event_type="On Campus",
+                                last_modified=datetime.datetime.fromisoformat(
+                                    "2023-06-29T09:41:17.367634+00:00"
+                                ),
+                                module_name="CSC1003[1] Computer Programming I",
+                                staff_member="Blott S",
+                                weeks=[3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                                group_name=None,
+                                parsed_name_data=[
+                                    models.ParsedNameData(
+                                        module_codes=["CSC1003"],
+                                        semester=models.Semester.SEMESTER_1,
+                                        delivery_type=models.DeliveryType.ON_CAMPUS,
+                                        activity_type=models.ActivityType.LECTURE,
+                                        group_number=1,
+                                    )
+                                ],
+                            )
+                        ],
+                    ],
+                ),
+            ],
+        ),
+        400: ResponseInfo(
+            description=(
+                "Bad Request.\n\nIf format was 'json', response will be in json format. Otherwise, response will be in plain text."
+            ),
+            content=[
+                ContentInfo(
+                    type=str,
+                    examples=["Cannot provide both course and modules."],
+                    content_type="text/plain",
+                ),
+                ContentInfo(
+                    type=models.APIError,
+                    examples=[
+                        models.APIError(400, "Cannot provide both course and modules.")
+                    ],
+                    content_type="application/json",
+                ),
+            ],
+        ),
+    },
+)
---
+++ backend/src/v2/server.py
@@ -0,0 +1,177 @@
+import collections
+import datetime
+import os
+
+import blacksheep
+import orjson
+from blacksheep.server.openapi.v3 import OpenAPIHandler
+from openapidocs.v3 import Info  # pyright: ignore[reportMissingTypeStubs]
+from timetable import api as api_
+from timetable import cns, models, utils
+
+from src import __version__, api_docs
+
+app = blacksheep.Application()
+api = api_.API(os.environ["REDIS_ADDRESS"])
+cns_api = cns.API(os.environ["CNS_ADDRESS"])
+
+docs = OpenAPIHandler(
+    info=Info(title="TimetableSync API", version=__version__),
+    ui_path="/api/api_docs",
+    json_spec_path="/api/openapi.json",
+)
+docs.bind_app(app)
+
+
+@app.on_start
+async def start_session() -> None:
+    for category_type in (
+        models.CategoryType.PROGRAMMES_OF_STUDY,
+        models.CategoryType.MODULES,
+        models.CategoryType.LOCATIONS,
+    ):
+        await utils.get_basic_category_results(api, category_type)
+
+
+@app.on_stop
+async def stop_session() -> None:
+    await api.session.close()
+    await cns_api.session.close()
+
+
+def str_to_list(text: str) -> list[str]:
+    return [t.strip() for t in text.split(",")]
+
+
+@docs.ignore()
+@blacksheep.route("/api/healthcheck")
+async def healthcheck() -> blacksheep.Response:
+    return blacksheep.ok()
+
+
+CATEGORY_TYPES: dict[str, models.CategoryType] = {
+    "course": models.CategoryType.PROGRAMMES_OF_STUDY,
+    "module": models.CategoryType.MODULES,
+    "location": models.CategoryType.LOCATIONS,
+}
+
+
+@docs.ignore()
+@blacksheep.route("/api/all/{category_type}")
+async def all_category_values(
+    category_type: str, query: str | None = None
+) -> blacksheep.Response:
+    if category_type not in ("course", "module", "location", "club", "society"):
+        return blacksheep.status_code(
+            400,
+            "Invalid value provided.",
+        )
+
+    if category_type in CATEGORY_TYPES:
+        categories = await utils.get_basic_category_results(
+            api, CATEGORY_TYPES[category_type], query
+        )
+
+    else:
+        categories = await cns_api.fetch_unlocked_groups(cns.GroupType(category_type))
+        if query:
+            categories = cns.filter_category_results(categories, query)
+
+    return blacksheep.Response(
+        status=200,
+        content=blacksheep.Content(
+            content_type=b"application/json",
+            data=orjson.dumps(categories),
+        ),
+    )
+
+
+@docs(api_docs.API)
+@blacksheep.route("/api")
+async def timetable_api(
+    course: str | None = None,  # NOTE: backwards compatibility only
+    courses: str | None = None,
+    modules: str | None = None,
+    locations: str | None = None,
+    format: str | None = None,
+    display: bool | None = None,
+    start: str | None = None,
+    end: str | None = None,
+) -> blacksheep.Response:
+    format_ = models.ResponseFormat.from_str(format if format else None)
+    start_date = datetime.datetime.fromisoformat(start) if start else None
+    end_date = datetime.datetime.fromisoformat(end) if end else None
+
+    if not course and not courses and not modules and not locations:
+        raise ValueError("No courses, modules or locations provided.")
+    if format_ is models.ResponseFormat.UNKNOWN:
+        raise ValueError(f"Invalid format '{format_}'.")
+
+    codes: dict[models.CategoryType, list[str]] = collections.defaultdict(list)
+
+    if courses and courses.strip():
+        codes[models.CategoryType.PROGRAMMES_OF_STUDY].extend(str_to_list(courses))
+    if course and course.strip() not in codes[models.CategoryType.PROGRAMMES_OF_STUDY]:
+        codes[models.CategoryType.PROGRAMMES_OF_STUDY].append(course.strip())
+    if modules:
+        codes[models.CategoryType.MODULES].extend(str_to_list(modules))
+    if locations:
+        codes[models.CategoryType.LOCATIONS].extend(str_to_list(locations))
+
+    items = await utils.resolve_to_category_items(codes, api)
+    identities = {
+        group: [item.identity for item in group_items]
+        for group, group_items in items.items()
+    }
+    events = await utils.gather_events(identities, start_date, end_date, api)
+
+    if format_ is models.ResponseFormat.ICAL:
+        timetable = utils.generate_ical_file(events)
+    else:
+        assert format_ is models.ResponseFormat.JSON
+        timetable = utils.generate_json_file(events, display)
+
+    return blacksheep.Response(
+        200,
+        content=blacksheep.Content(
+            format_.content_type.encode(),
+            data=timetable,
+        ),
+    )
+
+
+@blacksheep.route("/api/cns")
+async def cns_api_timetable(
+    societies: str | None = None,
+    clubs: str | None = None,
+):
+    if not (societies or clubs):
+        raise ValueError("No societies or clubs provided.")
+
+    society_ids = str_to_list(societies) if societies else []
+    club_ids = str_to_list(clubs) if clubs else []
+
+    events: dict[str, list[cns.Event | cns.Activity]] = collections.defaultdict(list)
+
+    for group_type, ids in (
+        (cns.GroupType.SOCIETY, society_ids),
+        (cns.GroupType.CLUB, club_ids),
+    ):
+        for id_ in ids:
+            group = await cns_api.fetch_group_info(group_type, id_)
+            events[group.name].extend(
+                await cns_api.fetch_group_events_activities(group_type, id_)
+            )
+
+    calendar = cns.generate_ical_file(events)
+
+    return blacksheep.Response(
+        200,
+        content=blacksheep.Content(
+            b"text/calendar",
+            data=calendar,
+        ),
+    )
+
+
+# TODO: add error handler
