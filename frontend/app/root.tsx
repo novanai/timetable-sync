2cfdb33e74c3a123528b6b3d22b5eb897ef9a8b0
nova <novanai.dev@gmail.com>
2026-01-17 14:17:40 +0000

    rework theme picker, replace SVGs with icons

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,3 +1,4 @@
+import type { Route } from "./+types/root";
 import { useEffect } from 'react';
 import { themeChange } from 'theme-change';
 import {
@@ -9,7 +10,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-import type { Route } from "./+types/root";
+import { FaSun, FaMoon, FaGithub } from 'react-icons/fa';
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -32,18 +34,26 @@ const pages = [
   { name: "API Documentation", path: "/api/docs", external: true },
 ]
 
-const themes = {
-  Light: [
-    { name: "Blue", value: "winter" },
-    { name: "Yellow", value: "bumblebee" },
-    { name: "Green", value: "emerald" },
-  ],
-  Dark: [
-    { name: "Blue", value: "night" },
-    { name: "Pink", value: "dracula" },
-    { name: "Green", value: "forest" },
-  ],
-};
+const themes = [
+  {
+    group: "Light",
+    icon: <FaSun size={48} />,
+    themeList: [
+      { name: "Blue", value: "winter" },
+      { name: "Yellow", value: "bumblebee" },
+      { name: "Green", value: "emerald" },
+    ],
+  },
+  {
+    group: "Dark",
+    icon: <FaMoon size={48} />,
+    themeList: [
+      { name: "Blue", value: "night" },
+      { name: "Pink", value: "dracula" },
+      { name: "Green", value: "forest" },
+    ],
+  },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -139,44 +149,39 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </div>
             <div className="navbar-end">
               <a href="https://github.com/novanai/timetable-sync" target="_blank" className="btn btn-ghost border-none hover:text-primary hover:bg-inherit hover:shadow-none">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 496 512"
-                  className="h-7 w-7 fill-current"
-                >
-                  <path
-                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                  />
-                </svg>
+                <FaGithub size={28} />
               </a>
               <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn hover:bg-base-300 border-none m-1">
                   Theme
-                  <svg
-                    width="12px"
-                    height="12px"
-                    className="inline-block h-2 w-2 fill-current opacity-60"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 2048 2048">
-                    <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-                  </svg>
+                  <MdOutlineKeyboardArrowDown />
                 </div>
                 <ul tabIndex={-1} className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
-                  {Object.entries(themes).map(([group, themeList]) => (
+                  {themes.map(({ group, icon, themeList }) => (
                     <div key={group}>
-                      <div className="divider">
-                        {group}
+                      <div className="divider gap-1">
+                        {icon}
+                        <span>{group}</span>
                       </div>
 
                       {themeList.map(({ name, value }) => (
                         <li key={value}>
-                          <input
-                            type="radio"
-                            name="theme-dropdown"
-                            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
-                            aria-label={name}
-                            data-set-theme={value}
-                          />
+                          <label className="btn btn-sm btn-block btn-ghost justify-start has-[input:checked]:bg-base-100">
+                            <input
+                              type="radio"
+                              name="theme-dropdown"
+                              className="theme-controller hidden"
+                              aria-label={name}
+                              data-set-theme={value}
+                            />
+
+                            <span
+                              data-theme={value}
+                              className="w-3 h-3 rounded-full bg-primary"
+                            />
+
+                            <span>{name}</span>
+                          </label>
                         </li>
                       ))}
                     </div>
